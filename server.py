f33b420bcae1020e3a664f546174f8296721d1a6
Gabriel <gabrielolobo42@gmail.com>
2022-02-09 02:19:54 -0300

    Creates Address

--- server.py
+++ server.py
@@ -4,5 +4,7 @@ import threading
 
 PORT = 3030
 SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
