2bdd2ea18364a87876d13114b13feb5bd2cd9eb5
Gabriel <gabrielolobo42@gmail.com>
2022-02-10 17:23:04 -0300

    adds disconnect and fix server.listen()

--- server.py
+++ server.py
@@ -6,27 +6,37 @@ PORT = 3030
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 HEADER = 128 #No. of Bits
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!QUIT!"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-#Allows code to start listening for connections.
-def start():
-    socket.listen()
+def start():#Allows code to start listening for connections.
+    server.listen()
+    print(f"[RUNNING] Server is Running on {SERVER}")
     while True:
         #Stores client's information.
         addr, conn = server.accept()
         thread = threading.Thread(target=client_manager, args=(addr, conn))
         thread.start()
-        #The ("number of threads" - 1) represents the number of connections to the server.
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() -1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() -1}") #The ("number of threads" - 1) represents the number of connections to the server.
 
 def client_manager(addr, conn):
     print(f"[NEW CONNECTION] {addr} is connected")
 
     connected = True
     while connected:
-        msg_length = conn.recv(HEADER)
+        msg_length = conn.recv(HEADER).decode(FORMAT) #Tells how long the message is.
+        msg_length = int(msg_length) #Converts to an integer
+        msg = conn.recv(msg_length).decode(FORMAT) #How many bytes per message.
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+        
+        print(f"[{addr}] {msg}")
+        
+    conn.close()
+
 
 print("[STARTING SERVER...]")
 start()
