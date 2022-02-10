b7ede22df399c1e00c3d02d4655c9a7b249b7ddb
Gabriel <gabrielolobo42@gmail.com>
2022-02-10 16:11:03 -0300

    add start and client_manager

--- server.py
+++ server.py
@@ -8,3 +8,15 @@ ADDR = (SERVER, PORT)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
+
+#Allows code to start listening for connections.
+def start():
+    socket.listen()
+    while True:
+        addr, conn = server.accept()
+
+def client_manager(addr, conn):
+    pass
+
+print("[STARTING SERVER...]")
+start()
